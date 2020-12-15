db9ea6a10a7ad865a7946840148f662d3dfb83f6
someCat <zhangtao@touchdata.io>
2020-12-15 12:01:01 +0800

    简化算法，增加浏览器是否支持的判断

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 let Notification = window.Notification
 
-const onerror = function (e) {}
-
-const onclick = function (e) {
-  e.preventDefault()
-  window.focus()
-  e.target.close()
-}
-
-const onclose = function (e) {}
-
-const onshow = function(e) {}
-
-const defaultEvents = {
-  onerror,
-  onclick,
-  onclose,
-  onshow
-}
+const TipText = function (data) {
+  return `[nativeNotificationOfVue]: ${data}`
+} 
 
 const NativeNotificationOfVue = {
   install: function (Vue, options) {
+    if (!Notification) {
+      console.error(TipText('Sorry, this browser does not support Notification API'))
+      return
+    }
     options = options || {}
+    const eventList = ['onerror', 'onclick', 'onclose', 'onshow']
     // 是否页面加载后马上询问通知权限，否则使用时会再询问
     options.requestOnLoad = !!options.requestOnLoad
 
@@ -36,21 +26,15 @@ const NativeNotificationOfVue = {
     }
     __prototype.$nativeNotification.requestPermission = requestPermission
     options.requestOnLoad && requestPermission()
-    // 主体函数
-    const push = function (params, e = {}) {
+    const push = function (params, outerEvents = {}) {
       if (typeof params !== 'object') {
-        throw new Error('[nativeNotification]: 参数格式错误')
+        console.error(TipText('Invalid arguments'))
       }
-      // title, // 通知标题
-      // body, // 通知内容
-      // tag, // 赋予通知一个ID，相同的ID情况下，后面的会覆盖前面的
-      // icon // 一个图片的URL，将被用于显示通知的图标
       let { title, body, tag, icon, data } = params
       
-      if (!e.onerror) e.onerror = function () { }
-      if (!e.onclick) e.onclick = function () { }
-      if (!e.onclose) e.onclose = function () { }
-      if (!e.onshow) e.onshow = function () { }
+      eventList.forEach(v => {
+        !outerEvents[v] && (outerEvents[v] = function () {})
+      })
       return Promise.resolve()
         .then(function () {
           if (Notification.permission !== 'granted') {
@@ -61,44 +45,31 @@ const NativeNotificationOfVue = {
         })
         .then(function (permission) {
           if (permission === 'denied') {
-            return new Error('无权发通知')
+            console.error(TipText('no access'))
+          }
+          
+          const bindEvents = {
+            onerror: function (event) { outerEvents.onerror(event) },
+            onclick: function (event) {
+              event.preventDefault()
+              window.focus()
+              event.target.close()
+              outerEvents.onclick(event)
+            },
+            onclose: function (event) { outerEvents.onclose(event) },
+            onshow: function (event) { outerEvents.onshow(event) }
           }
 
-          const bindError = function (event) {
-            'use strict'
-            defaultEvents.onerror(event)
-            e.onerror(event)
-          }
-
-          const bindClick = function (event) {
-            'use strict'
-            defaultEvents.onclick(event)
-            e.onclick(event)
-          }
-
-          const bindClose = function (event) {
-            'use strict'
-            defaultEvents.onclose(event)
-            e.onclose(event)
-          }
-
-          const bindShow = function (event) {
-            'use strict'
-            defaultEvents.onshow(event)
-            e.onshow(event)
-          }
 
           try {
             let notification = new Notification(title, {body, tag, icon, data})
-            notification.onerror = bindError
-            notification.onclick = bindClick
-            notification.onclose = bindClose
-            notification.onshow = bindShow
-
+            eventList.forEach(v => {
+              notification[v] = bindEvents[v]
+            })
             return notification
           } catch (e) {
             if (e.name !== 'TypeError') {
-              return e
+              console.error(TipText(e))
             }
 
             return navigator.serviceWorker.ready.then(
